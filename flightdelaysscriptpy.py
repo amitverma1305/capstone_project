4d68c4555b0441eb26ddc9b53cb5452414dfccd2
amitverma1305 <amitverma1305@gmail.com>
2020-04-21 18:08:07 -0700

    capstone project files

--- flightdelaysscriptpy.py
+++
@@ -1,4 +0,0 @@
- #! /usr/bin/env python3
-import pandas as pd
-flightdelay=pd.read_csv("flightdelays.csv")
-flightdelay[flightdelay['Origin']=='SFO']['ArrDelay'].iloc[:3]
--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import pandas as pd
-flightdelay=pd.read_csv("flightdelays.csv")
-data=flightdelay[flightdelay['Origin']=='SFO']['ArrDelay'].iloc[:3]
-data.to_csv('first3sfo.csv')
-c=pd.read_csv('first3sfo.csv',header=None)[1]
-print(c)
-print(flightdelay['Dest'].value_counts().head(3))
-print("Amit Verma")
